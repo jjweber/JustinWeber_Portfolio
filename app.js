d2dcce3487cc874d814a5f6452f97d71b8153e40
Your Name <you@example.com>
2018-03-22 23:47:51 -0400

    Added keywords to Meta Description element. Deployed on Heroku until issue with JustHost is solved.

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ app.locals.projectsData = require('./myWork.json');
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
